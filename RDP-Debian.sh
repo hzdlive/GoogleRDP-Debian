c2621ebeae11a6318babf9f1d3fce8b0bb3c8aa2
hzdlive <hzdlive@hotmail.com>
2021-06-24 20:44:53 +0800

    Update RDP-Debian.sh

--- RDP-Debian.sh
+++ RDP-Debian.sh
@@ -2,9 +2,9 @@
 printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m FSID
-sudo useradd -m AGID
-sudo adduser AGID sudo
-echo 'AGID:1234' | sudo chpasswd
+sudo useradd -m ZDID
+sudo adduser ZDID sudo
+echo 'ZDID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -19,13 +19,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser AGID chrome-remote-desktop
+sudo adduser ZDID chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - AGID -c """$CRP"""
+su - ZDID -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
